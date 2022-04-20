0232c87812aa94e93a30d1964de4b3a9104d02ef
Tanner <Tanner@DESKTOP-KINFF0F>
2022-04-20 11:33:41 -0600

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+Console.Write("Please Enter Your First Name > ");
+string userName = Console.ReadLine();
+
+Console.Write("Where are you from? > ");
+string location = Console.ReadLine();
+
+DateTime today = System.DateTime.Today;
+DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
+
+int daysToChristmas = (christmas - today).Days;
+
+Console.WriteLine($"My name is {userName} ");
+Console.WriteLine($"I am from {location}");
+Console.WriteLine($"Today is {today.ToShortDateString()}, there are {daysToChristmas} days until Christmas");
+Console.ReadKey();
