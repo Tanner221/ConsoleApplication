7d64d75e51426170a8878c7f27975cfdcba82a18
Tanner Robinson <Rob16041@byui.edu>
2022-04-21 13:15:19 -0600

    Addition of program class and main function

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
-Console.Write("Please Enter Your First Name > ");
-string userName = Console.ReadLine();
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please Enter Your First Name > ");
+            string userName = Console.ReadLine();
 
-Console.Write("Where are you from? > ");
-string location = Console.ReadLine();
+            Console.Write("Where are you from? > ");
+            string location = Console.ReadLine();
 
-DateTime today = System.DateTime.Today;
-DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
+            DateTime today = System.DateTime.Today;
+            DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
 
-int daysToChristmas = (christmas - today).Days;
+            int daysToChristmas = (christmas - today).Days;
 
-Console.WriteLine($"My name is {userName} ");
-Console.WriteLine($"I am from {location}");
-Console.WriteLine($"Today is {today.ToShortDateString()}, there are {daysToChristmas} days until Christmas");
-Console.ReadKey();
+            Console.WriteLine($"My name is {userName} ");
+            Console.WriteLine($"I am from {location}");
+            Console.WriteLine($"Today is {today.ToShortDateString()}, there are {daysToChristmas} days until Christmas");
+            Console.ReadKey();
+        }
+       
+    }
+}
