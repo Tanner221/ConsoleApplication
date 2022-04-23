aef6fd9ddc66b51155e73597c0b2e062762ed250
Tanner Robinson <Rob16041@byui.edu>
2022-04-23 16:49:14 -0600

    Added 2.1 Example

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -5,6 +5,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
+            // Step 2
             Console.Write("Please Enter Your First Name > ");
             string userName = Console.ReadLine();
 
@@ -12,7 +13,7 @@ namespace ConsoleApplication
             string location = Console.ReadLine();
 
             DateTime today = System.DateTime.Today;
-            DateTime christmas = new DateTime(DateTime.Now.Year, 12, 25);
+            DateTime christmas = new DateTime(today.Year, 12, 25);
 
             int daysToChristmas = (christmas - today).Days;
 
@@ -22,7 +23,26 @@ namespace ConsoleApplication
             Console.WriteLine($"My name is {userName} ");
             Console.WriteLine($"I am from {location}");
             Console.WriteLine($"Today is {today.ToShortDateString()}, there are {daysToChristmas} days until Christmas");
-            //pause
+            
+
+            //2.1 example
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.Write("Enter Width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.Write("Enter Height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
+            //End Program
+            Console.WriteLine("Press Any Key to End");
             Console.ReadKey();
         }
        
